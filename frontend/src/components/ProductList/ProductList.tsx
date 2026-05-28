a6ad842ada4ae7c359d81b709c7090d7b153b082
12PUffS <evgenypech98@gmail.com>
2026-05-28 15:07:50 +0300

    update banner

--- frontend/src/components/ProductList/ProductList.tsx
+++ frontend/src/components/ProductList/ProductList.tsx
@@ -163,7 +163,7 @@ export default function ProductList() {
 
             <ul className="card-list">
               {loading ? (
-                Array(10)
+                Array(20)
                   .fill(null)
                   .map((_, i) => (
                     <li key={i}>
